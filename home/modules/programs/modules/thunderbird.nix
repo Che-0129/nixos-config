a9be71db137768a22c66da076551379ff500e702
Che-0129 <yuto8664@gmail.com>
2025-11-22 04:41:55 +0900

    thunderbird langpack

--- home/modules/programs/modules/thunderbird.nix
+++ home/modules/programs/modules/thunderbird.nix
@@ -1,14 +1,20 @@
 { config, pkgs, ... }:
 
 {
-	programs.thunderbird = {
-		enable = true;
-		profiles.default = {
-			isDefault = true;
-			settings = {
-				"font.name.sans-serif.ja" = "Noto Sans CJK JP";
-				"intl.locale.requested" = "ja";
-			};
-		};
-	}
+    programs.thunderbird = {
+        enable = true;
+        package = pkgs.thunderbird.override {
+            extraPolicies.ExtensionSettings."langpack-ja@thunderbird.mozilla.org" = {
+                installation_mode = "normal_installed";
+                install_url = "https://addons.thunderbird.net/thunderbird/downloads/file/1042638/japanese_ja_language_pack-145.0.20251112.194829-tb.xpi?src=";
+            };
+        };
+        profiles.default = {
+            isDefault = true;
+            settings = {
+                "font.name.sans-serif.ja" = "Noto Sans CJK JP";
+                "intl.locale.requested" = "ja";
+            };
+        };
+    }
 }
