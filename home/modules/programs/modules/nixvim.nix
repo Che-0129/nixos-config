7678175425b14b8e8b8608b69acd948ca0f14f68
Che-0129 <yuto8664@gmail.com>
2025-12-11 08:52:41 +0900

    add nixvim settings

--- home/modules/programs/modules/nixvim.nix
+++ home/modules/programs/modules/nixvim.nix
@@ -23,7 +23,23 @@
             guicursor = "i:ver100-blinkon500-blinkoff500";
         };
         plugins = {
+            onenord.enable = true;
             lualine.enable = true;
+            smear-cursor = {
+                enable = true;
+                settings = {
+                    trailing_stiffness = 0.5;
+                    stiffness_insert_mode = 0.7;
+                    trailing_stiffness_insert_mode = 0.7;
+                    damping = 0.8;
+                    damping_insert_mode = 0.8;
+                    distance_stop_animating = 0.5;
+                };
+            };
+            neoscroll.enable = true;
+            whitespace-nvim.enable = true;
+            nvim-colorizer.enable = true;
+            which-key.enable = true;
         };
     };
 }
