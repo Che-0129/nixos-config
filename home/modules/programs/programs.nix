9a74fd7cb1629782706a7f743d0ed6afdb1f4b93
Che-0129 <yuto8664@gmail.com>
2026-02-27 17:23:25 +0900

    Add nh.nix and delete garbage-collection settings

--- home/modules/programs/programs.nix
+++ home/modules/programs/programs.nix
@@ -5,6 +5,7 @@
         ./modules/firefox.nix
         ./modules/fish.nix
         ./modules/foot.nix
+        ./modules/nh.nix
         ./modules/nixvim.nix
         ./modules/thunderbird.nix
         ./modules/waybar.nix
--- host/configuration.nix
+++ host/configuration.nix
@@ -33,19 +33,12 @@
         pam.services.hyprlock = {};
     };
 
-    nix = {
-        gc = {
-            automatic = true;
-            dates = "weekly";
-            options = "--delete-older-than 3d";
-        };
-        settings = {
-            auto-optimise-store = true;
-            experimental-features = [ "nix-command" "flakes" ];
-            substituters = ["https://hyprland.cachix.org"];
-            trusted-substituters = ["https://hyprland.cachix.org"];
-            trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
-        };
+    nix.settings = {
+        auto-optimise-store = true;
+        experimental-features = [ "nix-command" "flakes" ];
+        substituters = ["https://hyprland.cachix.org"];
+        trusted-substituters = ["https://hyprland.cachix.org"];
+        trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
     };
 
     nixpkgs.config.allowUnfree = true;
