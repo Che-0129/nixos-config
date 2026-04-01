3bdb77d5278d49eb246ed85a65f5a48d619c5f19
Che-0129 <yuto8664@gmail.com>
2026-04-01 16:54:31 +0900

    Remove unnecessary settings from networking.nix

--- configuration/modules/networking.nix
+++ configuration/modules/networking.nix
@@ -5,10 +5,7 @@
         useNetworkd = true;
         wireless.iwd = {
             enable = true;
-            settings = {
-                General.EnableNetworkConfiguration = true;
-                Network.NameResolvingService = "systemd";
-            };
+            settings.General.EnableNetworkConfiguration = true;
         };
     };
 }
