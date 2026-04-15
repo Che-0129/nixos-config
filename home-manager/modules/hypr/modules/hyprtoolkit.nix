b988dbb624a33fc091b55d75c339baf5c5a9aeef
Che-0129 <yuto8664@gmail.com>
2026-04-15 19:46:24 +0900

    hyprtoolkit.nix: Changed from xdg.configFile to home.file

--- home-manager/modules/hypr/modules/hyprtoolkit.nix
+++ home-manager/modules/hypr/modules/hyprtoolkit.nix
@@ -1,10 +1,12 @@
 { pkgs, ... }: {
-    home.packages = [ pkgs.hyprtoolkit ];
-    xdg.configFile."hypr/hyprtoolkit.conf".text = ''
-        background = rgba(383c4ae6)
-        accent = rgb(5294e2)
-        icon_theme = Adwaita
-        font_family = Noto Sans CJK JP
-        font_family_monospace = Moralerspace Neon HW
-    '';
+    home = {
+        packages = [ pkgs.hyprtoolkit ];
+        file.".config/hypr/hyprtoolkit.conf".text = ''
+            background = rgba(383c4ae6)
+            accent = rgb(5294e2)
+            icon_theme = Adwaita
+            font_family = Noto Sans CJK JP
+            font_family_monospace = Moralerspace Neon HW
+        '';
+    };
 }
