ccb6a11de78d5e56dadb68f0c921cd99a9f90502
Che-0129 <yuto8664@gmail.com>
2026-04-18 13:51:01 +0900

    Separating roles with networkd and iwd

--- configuration/modules/networking.nix
+++ configuration/modules/networking.nix
@@ -3,13 +3,6 @@
         dhcpcd.enable = false;
         hostName = "NixOS";
         useNetworkd = true;
-        wireless.iwd = {
-            enable = true;
-            settings = {
-                General.EnableNetworkConfiguration = true;
-                Network.NameResolvingService = "systemd";
-            };
-        };
+        wireless.iwd.enable = true;
     };
-    systemd.network.enable = true;
 }
