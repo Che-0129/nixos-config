e76902e86f692f3d8a0f2ac7515363dbc603a46c
Che-0129 <yuto8664@gmail.com>
2026-04-21 15:53:48 +0900

    add nix-community substituters

--- configuration/configuration.nix
+++ configuration/configuration.nix
@@ -13,6 +13,8 @@
     nix.settings = {
         auto-optimise-store = true;
         experimental-features = [ "nix-command" "flakes" ];
+        substituters = [ "https://nix-community.cachix.org" ];
+        trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
     };
     nixpkgs.config.allowUnfree = true;
     system.stateVersion = "26.05";
