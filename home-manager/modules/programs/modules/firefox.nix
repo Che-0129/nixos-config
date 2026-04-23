0443d673e125bc5433c70ef5ffafc4a917c2aba7
Che-0129 <yuto8664@gmail.com>
2026-04-23 18:55:46 +0900

    firefox.nix: edit userChrome

--- home-manager/modules/programs/modules/firefox.nix
+++ home-manager/modules/programs/modules/firefox.nix
@@ -24,57 +24,9 @@
                 "browser.tabs.closeWindowWithLastTab" = false;
                 "browser.toolbars.bookmarks.visibility" = "never";
                 "browser.uiCustomization.navBarWhenVerticalTabs" = [
-                    "back-button"
-                    "forward-button"
-                    "stop-reload-button"
-                    "customizableui-special-spring4"
                     "urlbar-container"
-                    "customizableui-special-spring3"
-                    "vertical-spacer"
                     "downloads-button"
                 ];
-                "browser.uiCustomization.state" = {
-                    placements = {
-                        widget-overflow-fixed-list = [];
-                        unified-extensions-area = [];
-                        nav-bar = [
-                            "back-button"
-                            "forward-button"
-                            "stop-reload-button"
-                            "customizableui-special-spring4"
-                            "urlbar-container"
-                            "customizableui-special-spring3"
-                            "vertical-spacer"
-                            "downloads-button"
-                        ];
-                        toolbar-menubar = [ "menubar-items" ];
-                        TabsToolbar = [];
-                        vertical-tabs = [ "tabbrowser-tabs" ];
-                        PersonalToolbar = [
-                            "import-button"
-                            "personal-bookmarks"
-                        ];
-                    };
-                    seen = [
-                        "developer-button"
-                        "screenshot-button"
-                        "jid1-zsmfwe4lcaw9oq_jetpack-browser-action"
-                        "_9350bc42-47fb-4598-ae0f-825e3dd9ceba_-browser-action"
-                        "enhancerforyoutube_maximerf_addons_mozilla_org-browser-action"
-                        "ublock0_raymondhill_net-browser-action"
-                        "x-dim-mode_juanbuis_com-browser-action"
-                    ];
-                    dirtyAreaCache = [
-                        "nav-bar"
-                        "TabsToolbar"
-                        "vertical-tabs"
-                        "PersonalToolbar"
-                        "toolbar-menubar"
-                        "unified-extensions-area"
-                    ];
-                    currentVersion = 23;
-                    newElementCount = 13;
-                };
                 "browser.urlbar.suggest" = false;
                 "browser.urlbar.trimURLs" = false;
                 "browser.quitShortcut.disabled" = true;
@@ -88,14 +40,17 @@
                 "ui.key.menuAccessKeyFocuses" = false;
             };
             userChrome = ''
-                moz-button[view="viewCustomizeSidebar"] {
-                    visibility: hidden;
-                }
+                .buttons-wrapper { min-height: 0 !important; }
+                #back-button,
+                #forward-button,
                 #identity-permission-box,
                 #picture-in-picture-button,
+                #reader-mode-button,
+                #sidebar-button,
                 #star-button-box,
                 #trust-icon-container,
-                #reader-mode-button {
+                #unified-extensions-button,
+                #vertical-spacer {
                     display: none !important;
                 }
             '';
