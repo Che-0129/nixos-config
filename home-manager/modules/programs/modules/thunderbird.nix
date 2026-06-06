78c110b09b14f5b4b1f4036c2bb57bf501566929
Che-0129 <yuto8664@gmail.com>
2026-06-06 18:18:19 +0900

    thunderbird.nix: Change the method for installing language packs

--- home-manager/modules/programs/modules/thunderbird.nix
+++ home-manager/modules/programs/modules/thunderbird.nix
@@ -1,19 +1,10 @@
-{ pkgs, ... }: {
+{ ... }: {
     programs.thunderbird = {
         enable = true;
-        package = pkgs.thunderbird.override {
-            extraPolicies.ExtensionSettings."langpack-ja@thunderbird.mozilla.org" = {
-                installation_mode = "normal_installed";
-                install_url = "https://addons.thunderbird.net/thunderbird/downloads/file/1044734/japanese_ja_language_pack-148.0.20260219.215726-tb.xpi";
-                updates_disabled = false;
-            };
-        };
+        languagePacks = [ "ja" ];
         profiles.che = {
             isDefault = true;
-            settings = {
-                "font.name.sans-serif.ja" = "Noto Sans CJK JP";
-                "intl.locale.requested" = "ja";
-            };
+            settings."font.name.sans-serif.ja" = "Noto Sans CJK JP";
         };
     };
 }
