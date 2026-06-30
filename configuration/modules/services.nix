916725b3342397592146ee2eb86a05f486c1fe24
Che-0129 <yuto8664@gmail.com>
2026-06-30 11:53:01 +0900

    remove udev rules

--- configuration/modules/services.nix
+++ configuration/modules/services.nix
@@ -1,10 +1,9 @@
-{ pkgs, ... }: {
+{ ... }: {
     services = {
         pipewire = {
             enable = true;
             pulse.enable = true;
             wireplumber.enable = true;
         };
-        udev.packages = [ pkgs.qmk-udev-rules ];
     };
 }
