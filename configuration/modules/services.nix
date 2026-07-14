ddeff0fd25fe8ef30e50c61c6a675fc2db95b57c
Che-0129 <yuto8664@gmail.com>
2026-07-14 21:20:32 +0900

    services.nix: Remove unnecessary indentation

--- configuration/modules/services.nix
+++ configuration/modules/services.nix
@@ -1,9 +1,7 @@
 { ... }: {
-    services = {
-        pipewire = {
-            enable = true;
-            pulse.enable = true;
-            wireplumber.enable = true;
-        };
+    services.pipewire = {
+        enable = true;
+        pulse.enable = true;
+        wireplumber.enable = true;
     };
 }
