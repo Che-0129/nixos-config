f9ad49f18a3abbd84eacb8085105dfb67f1aa1a8
Che-0129 <yuto8664@gmail.com>
2026-07-20 23:54:05 +0900

    home-manager.nix: add KiCad sessionVariables

--- home-manager/home-manager.nix
+++ home-manager/home-manager.nix
@@ -1,4 +1,4 @@
-{ ... }: {
+{ pkgs, ... }: {
     imports = [
         ./modules/gimp.nix
         ./modules/hypr/hypr.nix
@@ -8,7 +8,14 @@
         ./modules/services/services.nix
         ./modules/themes.nix
     ];
-    home.stateVersion = "26.11";
+    home = {
+        stateVersion = "26.11";
+        sessionVariables = {
+            KICAD10_SYMBOL_DIR = "${pkgs.kicad.libraries.symbols}/share/kicad/symbols";
+            KICAD10_FOOTPRINT_DIR = "${pkgs.kicad.libraries.footprints}/share/kicad/footprints";
+            KICAD10_TEMPLATE_DIR = "${pkgs.kicad.libraries.symbols}/share/kicad/template";
+        };
+    };
     xdg.userDirs = {
         enable = true;
         createDirectories = true;
