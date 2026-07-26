5a661fa8b2b75894768fffde7b3bb897743ed4e1
Che-0129 <yuto8664@gmail.com>
2026-07-27 00:32:36 +0900

    hardware-configuration.nix: Change kernel to LTS

--- configuration/modules/hardware-configuration.nix
+++ configuration/modules/hardware-configuration.nix
@@ -1,10 +1,9 @@
-{ config, lib, modulesPath, pkgs, ... }: {
+{ config, lib, modulesPath, ... }: {
     imports = [(modulesPath + "/installer/scan/not-detected.nix")];
     boot = {
         initrd.availableKernelModules = [ "nvme" "xhci_pci" "usb_storage" "usbhid" "sd_mod" ];
         kernel.sysctl."kernel.sysrq" = 1;
         kernelModules = [ "kvm-amd" ];
-        kernelPackages = pkgs.linuxPackages_zen;
         loader = {
             efi.canTouchEfiVariables = true;
             systemd-boot.enable = true;
