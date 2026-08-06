9cfe65b255ad185f63765f1f70386d6d93369d55
Che-0129 <yuto8664@gmail.com>
2026-08-06 18:15:50 +0900

    hyprlauncher.nix: Hide desktop entry for CLI app

--- home-manager/modules/hypr/modules/hyprlauncher.nix
+++ home-manager/modules/hypr/modules/hyprlauncher.nix
@@ -4,4 +4,14 @@
         package = hyprlauncher.packages.${pkgs.stdenv.hostPlatform.system}.hyprlauncher;
         settings.ui.window_size = "600 400";
     };
+    xdg.desktopEntries = {
+        "nvim" = {
+            name = "Neovim";
+            noDisplay = true;
+        };
+        "yazi" = {
+            name = "Yazi File Manager";
+            noDisplay = true;
+        };
+    };
 }
