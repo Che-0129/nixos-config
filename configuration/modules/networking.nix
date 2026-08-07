7d48d6ba2a8d43399d214edc2c509b00320405e8
Che-0129 <yuto8664@gmail.com>
2026-08-07 17:24:38 +0900

    networking.nix: Migrating from dhcpcd to systemd-networkd

--- configuration/modules/networking.nix
+++ configuration/modules/networking.nix
@@ -1,11 +1,22 @@
 { ... }: {
     networking = {
         hostName = "NixOS";
+        useNetworkd = true;
         wireless.iwd = {
             enable = true;
             settings = {
                 Network.EnableIPv6 = true;
                 Settings.AutoConnect = true;
+            };
+        };
+    };
+    systemd.network = {
+        enable = true;
+        networks."40-wlan" = {
+            matchConfig.Name = "wlan0";
+            networkConfig = {
+                DHCP = "yes";
+                IPv6AcceptRA = true;
             };
         };
     };
