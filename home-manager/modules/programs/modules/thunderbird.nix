445304ec866b6143ba937bbb62f870a0a27d2f56
Che-0129 <yuto8664@gmail.com>
2026-08-09 17:13:05 +0900

    thunderbird.nix: add theme setting

--- home-manager/modules/programs/modules/thunderbird.nix
+++ home-manager/modules/programs/modules/thunderbird.nix
@@ -2,6 +2,10 @@
     programs.thunderbird = {
         enable = true;
         languagePacks = [ "ja" ];
+        policies.ExtensionSettings."arc@dark" = {
+            install_url = "https://addons.thunderbird.net/thunderbird/downloads/latest/arc-x-dark/latest.xpi";
+            installation_mode = "normal_installed";
+        };
         profiles.che = {
             isDefault = true;
             settings."font.name.sans-serif.ja" = "Noto Sans CJK JP";
