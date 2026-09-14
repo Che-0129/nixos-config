80bcb8b12cbde0a07d30b91e3482e330ff1550af
Che-0129 <yuto8664@gmail.com>
2026-09-14 17:07:07 +0900

    syshud.nix: Move the contents of `home.file` to `services.syshud.style`

--- home-manager/modules/services/modules/syshud.nix
+++ home-manager/modules/services/modules/syshud.nix
@@ -6,22 +6,22 @@
             timeout = 1;
             transition-time = 250;
         };
+        style = ''
+            #syshud .box_layout { background: #313742; }
+            #syshud label { color: @theme_fg_color; }
+            #syshud scale trough {
+                border-radius: 4px;
+                opacity: 1.0;
+            }
+            #syshud scale slider { border: none; }
+            #syshud scale highlight {
+                background: #5294e2;
+                border: none;
+            }
+            #syshud .muted scale highlight { background: transparent; }
+            #syshud .muted label { color: alpha(@theme_fg_color, 0.5); }
+        '';
     };
-    home.file.".config/sys64/hud/style.css".text = ''
-        #syshud .box_layout { background: #313742; }
-        #syshud label { color: @theme_fg_color; }
-        #syshud scale trough {
-            border-radius: 4px;
-            opacity: 1.0;
-        }
-        #syshud scale slider { border: none; }
-        #syshud scale highlight {
-            background: #5294e2;
-            border: none;
-        }
-        #syshud .muted scale highlight { background: transparent; }
-        #syshud .muted label { color: alpha(@theme_fg_color, 0.5); }
-    '';
     systemd.user.services.syshud.Service.Environment = [
         "GTK_ICON_THEME=Adwaita"
         "XDG_DATA_DIRS=/run/current-system/sw/share"
