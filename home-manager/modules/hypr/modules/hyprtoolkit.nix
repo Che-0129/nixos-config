b217b7aa9492d892e6062ec55a1dd5e518c8692a
Che-0129 <yuto8664@gmail.com>
2026-09-14 16:37:32 +0900

    hyprtoolkit.nix: Change to use home-manager options

--- home-manager/modules/hypr/modules/hyprtoolkit.nix
+++ home-manager/modules/hypr/modules/hyprtoolkit.nix
@@ -1,13 +1,14 @@
 { inputs, pkgs, ... }: {
-    home = {
-        packages = [ inputs.hyprtoolkit.packages.${pkgs.stdenv.hostPlatform.system}.hyprtoolkit ];
-        file.".config/hypr/hyprtoolkit.conf".text = ''
-            background = rgb(383c4a)
-            accent = rgb(5294e2)
-            font_size = 14
-            icon_theme = Adwaita
-            font_family = Noto Sans CJK JP
-            font_family_monospace = Moralerspace Neon HW
-        '';
+    programs.hyprtoolkit = {
+        enable = true;
+        package = inputs.hyprtoolkit.packages.${pkgs.stdenv.hostPlatform.system}.hyprtoolkit;
+        settings = {
+            background = "0xFF383C4A";
+            accent = "0xFF5294E2";
+            font_size = 14;
+            icon_theme = "Adwaita";
+            font_family = "Noto Sans CJK JP";
+            font_family_monospace = "Moralerspace Neon HW";
+        };
     };
 }
