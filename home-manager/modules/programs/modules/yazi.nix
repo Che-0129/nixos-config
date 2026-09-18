b6f892358b9131be105d5ea6625a08a3de6577f2
Che-0129 <yuto8664@gmail.com>
2026-09-18 09:30:24 +0900

    yazi.nix: Add a setting to open audio and video files with VLC

--- home-manager/modules/programs/modules/yazi.nix
+++ home-manager/modules/programs/modules/yazi.nix
@@ -2,5 +2,22 @@
   programs.yazi = {
     enable = true;
     enableFishIntegration = true;
+    settings = {
+      open = {
+        rules = [
+          { mime = "video/*"; use = "vlc"; }
+          { mime = "audio/*"; use = "vlc"; }
+        ];
+      };
+      opener = {
+        vlc = [
+          {
+            run = "vlc --one-instance --started-from-file %s";
+            orphan = true;
+            for = "unix";
+          }
+        ];
+      };
+    };
   };
 }
