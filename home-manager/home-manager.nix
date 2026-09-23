bf4ed2f958e2867e0e615e406d31c4cfaf723350
Che-0129 <yuto8664@gmail.com>
2026-09-23 13:46:48 +0900

    Change file hierarchy

--- home-manager/home-manager.nix
+++ home-manager/home-manager.nix
@@ -1,12 +1,28 @@
 {
   imports = [
     ./modules/gimp.nix
-    ./modules/hypr/hypr.nix
-    ./modules/inputMethod/inputMethod.nix
+    ./modules/hypr/idle.nix
+    ./modules/hypr/land.nix
+    ./modules/hypr/launcher.nix
+    ./modules/hypr/lock.nix
+    ./modules/hypr/paper.nix
+    ./modules/hypr/polkitagent.nix
+    ./modules/hypr/toolkit.nix
+    ./modules/inputMethod/fcitx5.nix
     ./modules/inputMethod/mozc.nix
     ./modules/pkgs.nix
-    ./modules/programs/programs.nix
-    ./modules/services/services.nix
+    ./modules/programs/eza.nix
+    ./modules/programs/firefox.nix
+    ./modules/programs/fish.nix
+    ./modules/programs/foot.nix
+    ./modules/programs/nixvim.nix
+    ./modules/programs/thunderbird.nix
+    ./modules/programs/yazi.nix
+    ./modules/programs/zoxide.nix
+    ./modules/services/cliphist.nix
+    ./modules/services/playerctld.nix
+    ./modules/services/syshud.nix
+    ./modules/services/udiskie.nix
     ./modules/themes/themes.nix
   ];
   home.stateVersion = "26.11";
